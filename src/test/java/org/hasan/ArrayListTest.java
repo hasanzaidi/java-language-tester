b0f84e7ba5d84b6d4bf9e54248ab9706d3e56e03
Hasan Zaidi <hasan.zaidi@rocketmail.com>
2020-06-28 14:14:16 +0100

    Add some code for converting an ArrayList into an array of Strings.

--- src/test/java/org/hasan/ArrayListTest.java
+++ src/test/java/org/hasan/ArrayListTest.java
@@ -15,4 +15,16 @@ public class ArrayListTest {
         List<String> myList = new ArrayList<>();
         myList.add(3, "a");
     }
+
+    @Test
+    public void testConvertToArray() {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Orange");
+        fruits.add("Apple");
+        fruits.add("Banana");
+
+        String[] arr = fruits.toArray(new String[fruits.size()]);
+
+        assertThat(arr, is(new String[] {"Orange", "Apple", "Banana"}));
+    }
 }
