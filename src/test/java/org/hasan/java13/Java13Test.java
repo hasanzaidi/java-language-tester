544917d9f4a19be4c04b290498b139ead0f2acc7
Hasan Zaidi <hasan.zaidi@rocketmail.com>
2020-10-02 22:26:41 +0100

    Show Java 13 features.

---
+++ src/test/java/org/hasan/java13/Java13Test.java
@@ -0,0 +1,17 @@
+package org.hasan.java13;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Java13Test {
+    @Test
+    public void testMultilineStrings() {
+        String textBlock = """
+				Hi
+				Hello
+				Yes""";
+        assertThat(textBlock, is("Hi\nHello\nYes"));
+    }
+}
