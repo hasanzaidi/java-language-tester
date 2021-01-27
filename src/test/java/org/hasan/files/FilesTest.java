4caac37cea26aa5b419126cebe22819396a959d4
Hasan Zaidi <hasan.zaidi@rocketmail.com>
2021-01-27 17:37:26 +0000

    Add file test code

---
+++ src/test/java/org/hasan/files/FilesTest.java
@@ -0,0 +1,28 @@
+package org.hasan.files;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FilesTest {
+    @Test
+    public void testReadingFile() throws Exception {
+        // Given/When:
+        String content = readFromFile(this.getClass(), "/org/hasan/files/myfile.txt");
+
+        // Then:
+        assertThat(content, is("hello"));
+    }
+
+    private String readFromFile(Class testClass, String filename) throws IOException {
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(testClass.getResourceAsStream(filename)))) {
+            return r.lines().collect(Collectors.joining());
+        }
+    }
+}
