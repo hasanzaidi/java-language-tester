93a6d564790dc184043cebf52bcd9026db2f07c8
Hasan Zaidi <haszaidi@outlook.com>
2023-06-29 16:00:40 +0100

    Add example of list.copyOf.

--- src/test/java/org/hasan/java10/Java10Test.java
+++ src/test/java/org/hasan/java10/Java10Test.java
@@ -2,13 +2,33 @@ package org.hasan.java10;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 
 class Java10Test {
     @Test
     void testVarKeyword() {
         var myString = "hello world";
         assertThat(myString, is("hello world"));
+    }
+
+    @Test
+    void testCopyOf() {
+        // Given:
+        List<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+
+        // When:
+        // copyOf should only be used on a modifiable ArrayList
+        List<Integer> myUnmodifiableCopy = List.copyOf(myList);
+
+        // Then:
+        assertThat(myList, hasSize(2));
+        assertThat(myUnmodifiableCopy, hasSize(2));
     }
 }
