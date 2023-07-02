88eedf3ffdaf53053dfd7f1cb05c79b9c4bce00d
Hasan Zaidi <haszaidi@outlook.com>
2023-07-02 21:19:02 +0100

    Add a test for Supplier.

--- src/test/java/org/hasan/java8/SupplierTest.java
+++ src/test/java/org/hasan/java8/SupplierTest.java
@@ -2,6 +2,7 @@ package org.hasan.java8;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,5 +20,25 @@ class SupplierTest {
 
         // Then:
         assertThat(result, is(2.0));
+    }
+
+    @Test
+    void testSupplierLazy() {
+        // Given:
+        IntSupplier two = () -> 2;
+        // If this is executed then it would throw an exception
+        IntSupplier divisionByZero = () -> 1/0;
+
+        // When:
+        int result = add(two, divisionByZero);
+
+        // Then:
+        assertThat(result, is(2));
+    }
+
+    // Supplies are lazily evaluated so "unusedSupplier" is never executed as "getAsInt" is never called for it
+    private int add (IntSupplier supplier1, IntSupplier unusedSupplier) {
+        var actualX = supplier1.getAsInt();
+        return actualX;
     }
 }
