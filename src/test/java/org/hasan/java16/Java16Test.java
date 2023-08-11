8f256aa211c35adfa77be62803ed8b9dcf1293a8
Hasan Zaidi <haszaidi@outlook.com>
2023-08-11 23:03:17 +0100

    Add toList for Java 16.

--- src/test/java/org/hasan/java16/Java16Test.java
+++ src/test/java/org/hasan/java16/Java16Test.java
@@ -2,9 +2,12 @@ package org.hasan.java16;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Locale;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 
 class Java16Test {
@@ -15,5 +18,18 @@ class Java16Test {
         if (myObject instanceof String myString) {
             assertThat(myString.toLowerCase(Locale.ROOT), is("orange"));
         }
+    }
+
+    @Test
+    void toList() {
+        List<Integer> numbers = List.of(1, 2, 3).stream().toList();
+        assertThat(numbers, contains(1, 2, 3));
+    }
+
+    @Test
+    void toListIsImmutable() {
+        List<Integer> numbers = List.of(1, 2, 3).stream().toList();
+        assertThatThrownBy(() -> numbers.add(4))
+                .isInstanceOf(UnsupportedOperationException.class);
     }
 }
