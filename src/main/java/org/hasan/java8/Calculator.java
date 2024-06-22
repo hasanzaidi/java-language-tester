932d8d91b49b9c33e1aa4fc58531651e727399aa
Hasan Zaidi <haszaidi@outlook.com>
2024-06-22 20:20:53 +0100

    Add functional interface.

---
+++ src/main/java/org/hasan/java8/Calculator.java
@@ -0,0 +1,6 @@
+package org.hasan.java8;
+
+@FunctionalInterface
+interface Calculator {
+    int calculate(int x);
+}
---
+++ src/test/java/org/hasan/java8/FunctionalInterfaceTest.java
@@ -0,0 +1,20 @@
+package org.hasan.java8;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionalInterfaceTest {
+    @Test
+    void testSquare() {
+        // Given:
+        Calculator square = (int x) -> x * x;
+
+        // When:
+        int value = square.calculate(5);
+
+        // Then:
+        assertThat(value).isEqualTo(25);
+    }
+}
